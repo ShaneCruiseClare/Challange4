8fb97cc516af3c74b2943a8187d29f41ecfa345c
ShaneCruiseClare <114066730+ShaneCruiseClare@users.noreply.github.com>
2022-11-11 19:33:17 +0000

    newdoc

---
+++ Challenage4/Prototype 4/Assets/Script/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    public bool hasPowerup;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Powerup")){
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private float powerupStrength = 15.0f;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup) {
+
+            Rigidbody enemyRigibody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Player collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            enemyRigibody.AddForce(awayFromPlayer *powerupStrength, ForceMode.Impulse);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
